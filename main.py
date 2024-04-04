56d6dfaab18d1f4d7434c3174cb976cc294f5dd2
Drakinja <155208230+Drakinja@users.noreply.github.com>
2024-04-04 22:32:02 +0000

    Fix introduction and acquire if they need a new account

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-Program_Loop = True 
+#Welcome user to bot
+print('Welcome to ... Bank account manager bot.')
 
+#confirm if new user or not
+account_creation = input('User, would you like to create a new account, type Yes. If you already have one, type No. Be mindful it is caps sensitive.')
 
-
+if account_creation == 'Yes':
 #Acquire user info
-print('Hello, this program is here to help manage your bank account info')
+   print('Please input your required info for account creation in the below sections.')
 
-print('Please input your required info in the below sections.')
+   age = input('What is your age?')
+   name = input('Alright, what is your name?')
 
-in_use = True
-age = input('What is your age?')
-name = input('Alright, what is your name?')
+   print('So your name is {name} and your age is {age}. If this information is incorrect please restart the program.')
 
-user_confirmation = input(f'So your name is {name}, and your age is {age}. If correct type YES, if not type NO')
+   #Have them make a pin
+   pin = input('Please create a pin for your account. Do not share or spread this for safety of your account')
 
-if user_confirmation == 'YES' or user_confirmation == 'yes' or user_confirmation == 'Yes':
-      print('Okay, let us move on.')
+elif account_creation == 'No':
+     print(f'Alright, please enter your pin to access your account information.')
 
-elif user_confirmation == 'NO' or user_confirmation == 'no' or user_confirmation == 'No':
-     print('Alright, please re-enter the correct information.')
-  return in_use
-  
-#Have them make a pin
 
 
