069789aae62f60cdc043765bbb3a8a4778c496be
Drakinja <155208230+Drakinja@users.noreply.github.com>
2024-04-29 01:57:16 +0000

    Fixed line 65, 86  text & added else options in case of incorrect inputs

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-
 program_loop = True
-
 
 import mysql.connector
 connection = mysql.connector.connect(user = 'william', database = 'Local instance MySQL80', password = 'Dynamowill102008')
-
 
 #Welcome user to bot
 print('Welcome to ... Bank account manager bot.')
@@ -47,50 +44,55 @@ elif account_creation == 'No':
        print(item)
 
      cursor.close()
-  
+
+else:
+     print('This is not a correct response, please try again.')
+
 def display_menu():
     print("\n Here is what you can do In your account")
     print("1. Create savings")
     print("2. Check balance")
     print("3. Deposit Money")
     print("4. Withdraw Money")
-    print("5. close Program\n")
+    print("5. End Program & log out\n")
 
-def user_selection():
-      in_use = True
-      user_choice = input("Enter a number through 1-4: ")
+    def user_selection():
+        in_use = True
+        user_choice = input("Enter a number through 1-4: ")
 
-      if user_choice.isdigit():  # Check if the input consists of digits
-        user_choice = int(user_choice)
-        if 1 <= user_choice <= 5:
-            if user_choice == 1:
-              def savings_menu():
-                 print("\n **what type of savings would you like to make?**")
-                 print("1. Student Account")
-                 print("2. Retirement Account")
-                 print("3. Savings Account")
-                 print("4.  Return to Menu\n") 
-                 def savings_choice():
-                    if savings_choice.isdigit():    
-                      user_choice2 = int(savings_choice)
-                      if 1 <= user_choice <= 4:
-                          if savings_choice == 1:
-                              print()
-                          elif savings_choice == 2:
-                              print()
-                          elif savings_choice == 3:
-                              print()
-                          elif savings_choice == 4:
-                               return in_use
-            elif user_choice == 2:
-                 print(f'You have dollars in your account')
-            elif user_choice == 3:
-                 print('How much you youl like to deposit?')   
-            elif user_choice == 4:
-                 print('How much would you like')
-            elif user_choice == 5:    
-                 return in_use       
-
+        if user_choice.isdigit():  # Check if the input consists of digits
+          user_choice = int(user_choice)
+          if 1 <= user_choice <= 5: #Create functions below for each option of menu
+              if user_choice == 1:
+               def savings_menu():  #make a menu for savings
+                   print("\n **what type of account would you like to make?**")
+                   print("1. Student Account")
+                   print("2. Retirement Account")
+                   print("3. Savings Account")
+                   print("4.  Return to Menu\n")                    
+                   def savings_choice(): #function to go into savings choice options and management
+                       if savings_choice.isdigit():    
+                         savings_choice = int(savings_choice)
+                         if 1 <= savings_choice <= 4:
+                             if savings_choice == 1:
+                                 student_deposit = input('How much do you want to deposit in your Student account?')
+                             elif savings_choice == 2:
+                                 retirement_deposit = input('How much do you want to deposit in your Retirement Account?')
+                             elif savings_choice == 3:
+                                  savings_deposit = input('How much do you want to deposit in your Savings Account?')
+                             elif savings_choice == 4:
+                                  return in_use             
+              elif user_choice == 2: #Tell them balance
+                   print(f'You have a balance of dollars in your account')           
+              elif user_choice == 3: #Input added ammount & tell them new balance
+                  normal_deposit = input('How much you youl like to deposit?') 
+              elif user_choice == 4: #Input removed ammount & tell them new balance
+                  normal_withdraw = input('How much would you like to withdraw?')
+              elif user_choice == 5:
+                   in_use = False      
+              else: #create a function in case they make an invalid choice.
+                  print('This is not an available option.')
+                  return in_use 
 while program_loop:
   display_menu()
   in_use = user_selection()
